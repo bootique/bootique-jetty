dcc3e2d6136a87d6791d4a524c06fa9f3f684a52
Andrus Adamchik <andrus@objectstyle.com>
2016-03-15 11:23:51 -0400

    Upgrading to BQ 0.15 , refactoring unit tests

--- bootique-jetty/src/test/java/com/nhl/bootique/jetty/unit/JettyApp.java
+++ bootique-jetty/src/test/java/com/nhl/bootique/jetty/unit/JettyApp.java
@@ -1,35 +1,22 @@
 package com.nhl.bootique.jetty.unit;
 
-import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
 import org.eclipse.jetty.server.Server;
-import org.junit.rules.ExternalResource;
 
 import com.google.inject.Module;
 import com.nhl.bootique.BQRuntime;
 import com.nhl.bootique.Bootique;
 import com.nhl.bootique.jetty.JettyModule;
-import com.nhl.bootique.test.BQDaemonTestRuntime;
+import com.nhl.bootique.test.junit.BQDaemonTestFactory;
 
-public class JettyApp extends ExternalResource {
-
-	private BQDaemonTestRuntime app;
-
-	@Override
-	protected void after() {
-		if (app != null) {
-			BQDaemonTestRuntime localApp = app;
-			app = null;
-			localApp.stop();
-		}
-	}
+public class JettyApp extends BQDaemonTestFactory {
 
 	public void stop() {
 		after();
 	}
-	
+
 	public void start(Module config) {
 		startWithArgs(config, "--server");
 	}
@@ -41,7 +28,6 @@ public class JettyApp extends ExternalResource {
 		};
 		Function<BQRuntime, Boolean> startupCheck = r -> r.getInstance(Server.class).isStarted();
 
-		this.app = new BQDaemonTestRuntime(configurator, startupCheck);
-		this.app.start(5, TimeUnit.SECONDS, args);
+		newRuntime().configurator(configurator).startupCheck(startupCheck).start(args);
 	}
 }
