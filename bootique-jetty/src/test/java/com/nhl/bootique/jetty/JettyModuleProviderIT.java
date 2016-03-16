c2430df93ce40a0e3036c67f637acf7c7eeaf29c
Andrus Adamchik <andrus@objectstyle.com>
2016-03-16 19:41:26 -0400

    refactoring module provider tests per BQ #44

--- bootique-jetty/src/test/java/com/nhl/bootique/jetty/JettyModuleProviderIT.java
+++ bootique-jetty/src/test/java/com/nhl/bootique/jetty/JettyModuleProviderIT.java
@@ -1,21 +1,13 @@
 package com.nhl.bootique.jetty;
 
-import static java.util.stream.Collectors.counting;
-import static org.junit.Assert.assertEquals;
-
-import java.util.ServiceLoader;
-import java.util.stream.StreamSupport;
-
 import org.junit.Test;
 
-import com.nhl.bootique.BQModuleProvider;
+import com.nhl.bootique.test.junit.BQModuleProviderChecker;
 
 public class JettyModuleProviderIT {
 
 	@Test
 	public void testPresentInJar() {
-		long c = StreamSupport.stream(ServiceLoader.load(BQModuleProvider.class).spliterator(), false)
-				.filter(p -> p instanceof JettyModuleProvider).collect(counting());
-		assertEquals("No provider found", 1, c);
+		BQModuleProviderChecker.testPresentInJar(JettyModuleProvider.class);
 	}
 }
