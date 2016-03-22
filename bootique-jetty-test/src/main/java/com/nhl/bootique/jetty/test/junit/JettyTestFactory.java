d7ef59be1c5560aec8d8e7adafca7987c95014c5
Andrus Adamchik <andrus@objectstyle.com>
2016-03-22 16:03:25 +0300

    Jetty test module #14

--- bootique-jetty-test/src/main/java/com/nhl/bootique/jetty/test/junit/JettyTestFactory.java
+++ bootique-jetty-test/src/main/java/com/nhl/bootique/jetty/test/junit/JettyTestFactory.java
@@ -1,5 +1,8 @@
 package com.nhl.bootique.jetty.test.junit;
 
+import java.util.Collection;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 import org.eclipse.jetty.server.Server;
@@ -7,7 +10,9 @@ import org.junit.ClassRule;
 import org.junit.Rule;
 
 import com.nhl.bootique.BQRuntime;
+import com.nhl.bootique.Bootique;
 import com.nhl.bootique.jetty.JettyModule;
+import com.nhl.bootique.test.BQDaemonTestRuntime;
 import com.nhl.bootique.test.junit.BQDaemonTestFactory;
 
 /**
@@ -32,7 +37,51 @@ public class JettyTestFactory extends BQDaemonTestFactory {
 	@Override
 	public Builder newRuntime() {
 		Function<BQRuntime, Boolean> startupCheck = r -> r.getInstance(Server.class).isStarted();
-		return super.newRuntime().startupCheck(startupCheck)
+		return new Builder(runtimes).startupCheck(startupCheck)
 				.configurator(bootique -> bootique.module(JettyModule.class));
+	}
+
+	public static class Builder extends com.nhl.bootique.test.junit.BQDaemonTestFactory.Builder {
+
+		public Builder(Collection<BQDaemonTestRuntime> runtimes) {
+			super(runtimes);
+		}
+
+		@Override
+		public Builder startupCheck(Function<BQRuntime, Boolean> startupCheck) {
+			super.startupCheck(startupCheck);
+			return this;
+		}
+
+		@Override
+		public Builder configurator(Consumer<Bootique> configurator) {
+			super.configurator(configurator);
+			return this;
+		}
+
+		@Override
+		public Builder property(String key, String value) {
+			super.property(key, value);
+			return this;
+		}
+
+		@Override
+		public Builder startupTimeout(long timeout, TimeUnit unit) {
+			super.startupTimeout(timeout, unit);
+			return this;
+		}
+
+		public void startServer(String... args) {
+
+			int len = args != null ? args.length + 1 : 1;
+			String[] newArgs = new String[len];
+			newArgs[0] = "--server";
+
+			if (len > 1) {
+				System.arraycopy(args, 0, newArgs, 1, len - 1);
+			}
+
+			start(newArgs);
+		}
 	}
 }
