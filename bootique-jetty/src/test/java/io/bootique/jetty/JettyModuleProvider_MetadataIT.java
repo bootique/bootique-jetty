74f7c1c625e9d914d5c33db57b3bf681cd1b1cba
Andrus Adamchik <andrus@objectstyle.com>
2017-01-03 17:37:24 +0300

    Upgrade to Bootique 0.21: new app metadata package, annotated config help #56
    * adding metadata unit tests

--- bootique-jetty/src/test/java/io/bootique/jetty/JettyModuleProvider_MetadataIT.java
+++ bootique-jetty/src/test/java/io/bootique/jetty/JettyModuleProvider_MetadataIT.java
@@ -1,5 +1,11 @@
 package io.bootique.jetty;
 
+import io.bootique.meta.config.ConfigListMetadata;
+import io.bootique.meta.config.ConfigMapMetadata;
+import io.bootique.meta.config.ConfigMetadataNode;
+import io.bootique.meta.config.ConfigMetadataVisitor;
+import io.bootique.meta.config.ConfigObjectMetadata;
+import io.bootique.meta.config.ConfigValueMetadata;
 import io.bootique.meta.module.ModuleMetadata;
 import io.bootique.meta.module.ModulesMetadata;
 import io.bootique.test.BQTestRuntime;
@@ -7,8 +13,10 @@ import io.bootique.test.junit.BQTestFactory;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.Comparator;
 import java.util.Optional;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class JettyModuleProvider_MetadataIT {
@@ -31,5 +39,55 @@ public class JettyModuleProvider_MetadataIT {
         ModuleMetadata jetty = jettyOpt.get();
 
         assertTrue(jetty.getDescription().startsWith("Integrates Jetty web server"));
+
+        assertEquals(1, jetty.getConfigs().size());
+        ConfigMetadataNode rootConfig = jetty.getConfigs().stream().findFirst().get();
+
+        assertEquals("jetty", rootConfig.getName());
+
+        String result = rootConfig.accept(new ConfigMetadataVisitor<String>() {
+
+            @Override
+            public String visitObjectMetadata(ConfigObjectMetadata metadata) {
+
+                StringBuilder out = new StringBuilder(metadata.getName());
+
+                metadata.getProperties()
+                        .stream()
+                        .sorted(Comparator.comparing(ConfigMetadataNode::getName))
+                        .forEach(p -> out.append("[").append(p.accept(this)).append("]"));
+
+                return out.toString();
+            }
+
+            @Override
+            public String visitValueMetadata(ConfigValueMetadata metadata) {
+                return metadata.getName() + ":" + metadata.getType().getTypeName();
+            }
+
+            @Override
+            public String visitListMetadata(ConfigListMetadata metadata) {
+                return "list:" + metadata.getName();
+            }
+
+            @Override
+            public String visitMapMetadata(ConfigMapMetadata metadata) {
+                return "map:" + metadata.getName();
+            }
+        });
+
+        assertEquals("jetty" +
+                "[compression:boolean]" +
+                "[list:connectors]" +
+                "[context:java.lang.String]" +
+                "[map:filters]" +
+                "[idleThreadTimeout:int]" +
+                "[maxQueuedRequests:int]" +
+                "[maxThreads:int]" +
+                "[minThreads:int]" +
+                "[map:params]" +
+                "[map:servlets]" +
+                "[sessions:boolean]" +
+                "[staticResourceBase:io.bootique.resource.FolderResourceFactory]", result);
     }
 }
