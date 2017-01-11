1fd6f772f0638dc411bfb56098c155d0252f7ee9
Andrus Adamchik <andrus@objectstyle.com>
2017-01-11 14:04:32 -0500

    Upgrade to Bootique 0.21: new app metadata package, annotated config help #56

--- bootique-jetty/src/main/java/io/bootique/jetty/JettyModuleProvider.java
+++ bootique-jetty/src/main/java/io/bootique/jetty/JettyModuleProvider.java
@@ -5,6 +5,7 @@ import io.bootique.BQModule;
 import io.bootique.BQModuleProvider;
 import io.bootique.jetty.server.ServerFactory;
 
+import java.lang.reflect.Type;
 import java.util.Collections;
 import java.util.Map;
 
@@ -20,7 +21,7 @@ public class JettyModuleProvider implements BQModuleProvider {
 	 * @return a single entry map with {@link ServerFactory}.
 	 */
 	@Override
-	public Map<String, Class<?>> configs() {
+	public Map<String, Type> configs() {
 		// TODO: config prefix is hardcoded. Refactor away from ConfigModule, and make provider
 		// generate config prefix, reusing it in metadata...
 		return Collections.singletonMap("jetty", ServerFactory.class);
