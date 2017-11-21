f35a47c81d5cf9f4c9ecdde2b200e2a31171a21a
Andrus Adamchik <andrus@objectstyle.com>
2017-11-21 17:49:07 +0300

    a few syntax shorthands

--- bootique-jetty-instrumented/src/main/java/io/bootique/jetty/instrumented/server/InstrumentedQueuedThreadPool.java
+++ bootique-jetty-instrumented/src/main/java/io/bootique/jetty/instrumented/server/InstrumentedQueuedThreadPool.java
@@ -11,82 +11,70 @@ import java.util.concurrent.BlockingQueue;
 // https://github.com/dropwizard/metrics/blob/master/metrics-jetty9/src/main/java/io/dropwizard/metrics/jetty9/InstrumentedQueuedThreadPool.java
 public class InstrumentedQueuedThreadPool extends QueuedThreadPool {
 
-	private MetricRegistry metricRegistry;
+    private MetricRegistry metricRegistry;
 
-	public InstrumentedQueuedThreadPool(MetricRegistry metricRegistry) {
-		super();
-		this.metricRegistry = metricRegistry;
-	}
+    public InstrumentedQueuedThreadPool(MetricRegistry metricRegistry) {
+        super();
+        this.metricRegistry = metricRegistry;
+    }
 
-	public InstrumentedQueuedThreadPool(int maxThreads, int minThreads, int idleTimeout, BlockingQueue<Runnable> queue,
-			ThreadGroup threadGroup, MetricRegistry metricRegistry) {
-		super(maxThreads, minThreads, idleTimeout, queue, threadGroup);
-		this.metricRegistry = metricRegistry;
-	}
+    public InstrumentedQueuedThreadPool(int maxThreads, int minThreads, int idleTimeout, BlockingQueue<Runnable> queue,
+                                        ThreadGroup threadGroup, MetricRegistry metricRegistry) {
+        super(maxThreads, minThreads, idleTimeout, queue, threadGroup);
+        this.metricRegistry = metricRegistry;
+    }
 
-	public InstrumentedQueuedThreadPool(int maxThreads, int minThreads, int idleTimeout, BlockingQueue<Runnable> queue,
-			MetricRegistry metricRegistry) {
-		super(maxThreads, minThreads, idleTimeout, queue);
-		this.metricRegistry = metricRegistry;
-	}
+    public InstrumentedQueuedThreadPool(int maxThreads, int minThreads, int idleTimeout, BlockingQueue<Runnable> queue,
+                                        MetricRegistry metricRegistry) {
+        super(maxThreads, minThreads, idleTimeout, queue);
+        this.metricRegistry = metricRegistry;
+    }
 
-	public InstrumentedQueuedThreadPool(int maxThreads, int minThreads, int idleTimeout,
-			MetricRegistry metricRegistry) {
-		super(maxThreads, minThreads, idleTimeout);
-		this.metricRegistry = metricRegistry;
-	}
+    public InstrumentedQueuedThreadPool(int maxThreads, int minThreads, int idleTimeout,
+                                        MetricRegistry metricRegistry) {
+        super(maxThreads, minThreads, idleTimeout);
+        this.metricRegistry = metricRegistry;
+    }
 
-	public InstrumentedQueuedThreadPool(int maxThreads, int minThreads, MetricRegistry metricRegistry) {
-		super(maxThreads, minThreads);
-		this.metricRegistry = metricRegistry;
-	}
+    public InstrumentedQueuedThreadPool(int maxThreads, int minThreads, MetricRegistry metricRegistry) {
+        super(maxThreads, minThreads);
+        this.metricRegistry = metricRegistry;
+    }
 
-	public InstrumentedQueuedThreadPool(int maxThreads, MetricRegistry metricRegistry) {
-		super(maxThreads);
-		this.metricRegistry = metricRegistry;
-	}
+    public InstrumentedQueuedThreadPool(int maxThreads, MetricRegistry metricRegistry) {
+        super(maxThreads);
+        this.metricRegistry = metricRegistry;
+    }
 
-	@Override
-	protected void doStart() throws Exception {
+    @Override
+    protected void doStart() throws Exception {
 
-		super.doStart();
+        super.doStart();
 
-		metricRegistry.register(MetricRegistry.name(QueuedThreadPool.class, getName(), "utilization"),
-				new RatioGauge() {
-					@Override
-					protected Ratio getRatio() {
-						int threads = getThreads();
-						return Ratio.of(threads - getIdleThreads(), threads);
-					}
-				});
-		
-		metricRegistry.register(MetricRegistry.name(QueuedThreadPool.class, getName(), "utilization-max"),
-				new RatioGauge() {
-					@Override
-					protected Ratio getRatio() {
-						return Ratio.of(getThreads() - getIdleThreads(), getMaxThreads());
-					}
-				});
-		
-		metricRegistry.register(MetricRegistry.name(QueuedThreadPool.class, getName(), "size"), new Gauge<Integer>() {
-			
-			@Override
-			public Integer getValue() {
-				return getThreads();
-			}
-		});
-		
-		metricRegistry.register(MetricRegistry.name(QueuedThreadPool.class, getName(), "queued-requests"), new Gauge<Integer>() {
-			
-			@Override
-			public Integer getValue() {
-				// This assumes the QueuedThreadPool is using a
-				// BlockingArrayQueue or
-				// ArrayBlockingQueue for its queue, and is therefore a
-				// constant-time operation.
-				return getQueue().size();
-			}
-		});
-	}
+        metricRegistry.register(MetricRegistry.name(QueuedThreadPool.class, getName(), "utilization"),
+                new RatioGauge() {
+                    @Override
+                    protected Ratio getRatio() {
+                        int threads = getThreads();
+                        return Ratio.of(threads - getIdleThreads(), threads);
+                    }
+                });
+
+        metricRegistry.register(MetricRegistry.name(QueuedThreadPool.class, getName(), "utilization-max"),
+                new RatioGauge() {
+                    @Override
+                    protected Ratio getRatio() {
+                        return Ratio.of(getThreads() - getIdleThreads(), getMaxThreads());
+                    }
+                });
+
+        metricRegistry.register(MetricRegistry.name(QueuedThreadPool.class, getName(), "size"),
+                (Gauge<Integer>) this::getThreads);
+
+        metricRegistry.register(MetricRegistry.name(QueuedThreadPool.class, getName(), "queued-requests"),
+                // This assumes the QueuedThreadPool is using a BlockingArrayQueue or ArrayBlockingQueue for its queue,
+                // and is therefore a constant-time operation.
+                (Gauge<Integer>) () -> getQueue().size());
+    }
 
 }
