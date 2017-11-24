5cf3cf21f2a17009385c4b26a229532bcac57b31
Andrus Adamchik <andrus@objectstyle.com>
2017-11-24 11:14:58 +0300

    fixing race condition

--- bootique-jetty-instrumented/src/test/java/io/bootique/jetty/instrumented/server/ThreadPoolMetricsIT.java
+++ bootique-jetty-instrumented/src/test/java/io/bootique/jetty/instrumented/server/ThreadPoolMetricsIT.java
@@ -10,7 +10,6 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
 import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
@@ -67,22 +66,16 @@ public class ThreadPoolMetricsIT {
     }
 
     private void checkQueued(BQRuntime runtime, int frozenRequests) {
-
         Gauge<Integer> gauge = findQueuedRequestsGauge(runtime);
-
-        // we don't have a latch to wait till requests queuing is done, so assert and wait and assert again.
         assertWithRetry(() -> assertEquals(Integer.valueOf(frozenRequests), gauge.getValue()));
     }
 
     private void checkUtilizationVsMax(BQRuntime runtime, int frozenRequests) {
         Gauge<Double> gauge = findUtilizationVsMaxGauge(runtime);
 
-        // debug race conditions (we saw some on Travis)
-        dumpJettyThreads();
-
         // utilizationMax = (acceptorTh + selectorTh + active) / max
         // see more detailed explanation in InstrumentedQueuedThreadPool
-        assertEquals((2 + 3 + frozenRequests) / 20d, gauge.getValue(), 0.0001);
+        assertWithRetry(() -> assertEquals((2 + 3 + frozenRequests) / 20d, gauge.getValue(), 0.0001));
     }
 
     private void assertWithRetry(Runnable test) {
@@ -103,25 +96,6 @@ public class ThreadPoolMetricsIT {
 
         // fail for real
         test.run();
-    }
-
-    private void dumpJettyThreads() {
-
-        // debugging travis failures...
-        ThreadGroup tg = Thread.currentThread().getThreadGroup();
-        while (tg.getParent() != null) {
-            tg = tg.getParent();
-        }
-
-        Thread[] active = new Thread[tg.activeCount()];
-        tg.enumerate(active);
-
-        // there is a small chance a thread becomes inactive between 'activeCount' and 'enumerate' calls,
-        // resulting in null threads in the array.. remove null threads from the result
-        Arrays.stream(active).filter(t -> t != null && t.getName()
-                .startsWith("bootique-http"))
-                .map(t -> t.getName() + " - " + t.getState())
-                .forEach(LOGGER::info);
     }
 
     private Gauge<Double> findUtilizationVsMaxGauge(BQRuntime runtime) {
