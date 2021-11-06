710c038ac6d180ac7f91fe083566f22bc23a59c1
Andrus Adamchik <andrus@objectstyle.com>
2021-11-06 16:47:26 +0200

    Jetty 11 support #111
    .. porting instrumented module

---
+++ bootique-jetty11-instrumented/src/main/java/io/bootique/jetty/v11/instrumented/server/InstrumentedQueuedThreadPool.java
@@ -0,0 +1,136 @@
+/**
+ *  Licensed to ObjectStyle LLC under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ObjectStyle LLC licenses
+ *  this file to you under the Apache License, Version 2.0 (the
+ *  “License”); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  “AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package io.bootique.jetty.v11.instrumented.server;
+
+import com.codahale.metrics.Gauge;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.RatioGauge;
+import io.bootique.jetty.v11.instrumented.JettyInstrumentedModule;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
+
+import java.util.concurrent.BlockingQueue;
+
+public class InstrumentedQueuedThreadPool extends QueuedThreadPool {
+
+    // for now assuming a single thread pool, so we can hardcode its name
+    private static final String sizeMetric = JettyInstrumentedModule
+            .METRIC_NAMING
+            .name("ThreadPool", "Size");
+
+    private static final String queuedRequestsMetric = JettyInstrumentedModule
+            .METRIC_NAMING
+            .name("ThreadPool", "QueuedRequests");
+
+    private static final String utilization = JettyInstrumentedModule
+            .METRIC_NAMING
+            .name("ThreadPool", "Utilization");
+
+    private MetricRegistry metricRegistry;
+
+    public InstrumentedQueuedThreadPool(MetricRegistry metricRegistry) {
+        super();
+        this.metricRegistry = metricRegistry;
+    }
+
+    public InstrumentedQueuedThreadPool(
+            int maxThreads,
+            int minThreads,
+            int idleTimeout,
+            BlockingQueue<Runnable> queue,
+            ThreadGroup threadGroup,
+            MetricRegistry metricRegistry) {
+
+        super(maxThreads, minThreads, idleTimeout, queue, threadGroup);
+        this.metricRegistry = metricRegistry;
+    }
+
+    public InstrumentedQueuedThreadPool(
+            int maxThreads,
+            int minThreads,
+            int idleTimeout,
+            BlockingQueue<Runnable> queue,
+            MetricRegistry metricRegistry) {
+
+        super(maxThreads, minThreads, idleTimeout, queue);
+        this.metricRegistry = metricRegistry;
+    }
+
+    public InstrumentedQueuedThreadPool(
+            int maxThreads,
+            int minThreads,
+            int idleTimeout,
+            MetricRegistry metricRegistry) {
+
+        super(maxThreads, minThreads, idleTimeout);
+        this.metricRegistry = metricRegistry;
+    }
+
+    public InstrumentedQueuedThreadPool(
+            int maxThreads,
+            int minThreads,
+            MetricRegistry metricRegistry) {
+
+        super(maxThreads, minThreads);
+        this.metricRegistry = metricRegistry;
+    }
+
+    public InstrumentedQueuedThreadPool(
+            int maxThreads,
+            MetricRegistry metricRegistry) {
+
+        super(maxThreads);
+        this.metricRegistry = metricRegistry;
+    }
+
+    public static String sizeMetric() {
+        return sizeMetric;
+    }
+
+    public static String queuedRequestsMetric() {
+        return queuedRequestsMetric;
+    }
+
+    public static String utilizationMetric() {
+        return utilization;
+    }
+
+    @Override
+    protected void doStart() throws Exception {
+
+        super.doStart();
+
+        metricRegistry.register(sizeMetric(), (Gauge<Integer>) this::getThreads);
+        metricRegistry.register(queuedRequestsMetric(), (Gauge<Integer>) this::getQueueSize);
+        metricRegistry.register(utilizationMetric(), (Gauge<Double>) this::getUtilization);
+    }
+
+    protected double getUtilization() {
+
+        // utilization is:
+        //     (all_acceptor_t + all_selector_t + active_request_t) / maxT
+
+        // This is not readily apparent from the Jetty API below. An explanation:
+        //     getThreads()                    == all_acceptor_t + all_selector_t + active_request_t + idle_request_t
+        // hence
+        //     getThreads() - getIdleThreads() == all_acceptor_t + all_selector_t + active_request_t
+
+        return RatioGauge.Ratio.of(getThreads() - getIdleThreads(), getMaxThreads()).getValue();
+    }
+}
---
+++ bootique-jetty11-instrumented/src/test/java/io/bootique/jetty/v11/instrumented/request/RequestTimerIT.java
@@ -0,0 +1,90 @@
+/**
+ *  Licensed to ObjectStyle LLC under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ObjectStyle LLC licenses
+ *  this file to you under the Apache License, Version 2.0 (the
+ *  “License”); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  “AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package io.bootique.jetty.v11.instrumented.request;
+
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
+import io.bootique.BQRuntime;
+import io.bootique.jetty.v11.JettyModule;
+import io.bootique.jetty.v11.junit5.JettyTester;
+import io.bootique.junit5.BQTest;
+import io.bootique.junit5.BQTestFactory;
+import io.bootique.junit5.BQTestTool;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import java.io.IOException;
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@BQTest
+public class RequestTimerIT {
+
+    @BQTestTool
+    final BQTestFactory testFactory = new BQTestFactory().autoLoadModules();
+
+    final JettyTester tester = JettyTester.create();
+
+    @Test
+    public void testInitParametersPassed() {
+
+        BQRuntime runtime = testFactory.app("-s")
+                .module(b -> JettyModule.extend(b).addServlet(new TestServlet(), "s1", "/*"))
+                .module(tester.moduleReplacingConnectors())
+                .createRuntime();
+
+        runtime.run();
+
+        WebTarget target = tester.getTarget();
+
+        Response r1 = target.request().get();
+        assertEquals(Status.OK.getStatusCode(), r1.getStatus());
+
+        assertEquals("test_servlet", r1.readEntity(String.class));
+
+        MetricRegistry metrics = runtime.getInstance(MetricRegistry.class);
+
+        Collection<Timer> timers = metrics.getTimers().values();
+        assertEquals(1, timers.size());
+
+        Timer timer = timers.iterator().next();
+        assertEquals(1, timer.getCount());
+
+        target.request().get().close();
+        assertEquals(2, timer.getCount());
+    }
+
+    static class TestServlet extends HttpServlet {
+
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            resp.setContentType("text/plain");
+            resp.getWriter().print("test_servlet");
+        }
+    }
+
+}
