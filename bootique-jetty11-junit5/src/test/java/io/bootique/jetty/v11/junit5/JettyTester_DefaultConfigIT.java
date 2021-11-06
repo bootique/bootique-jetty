5ea959c717e80df0706fafe074f8b400a5f7d597
Andrus Adamchik <andrus@objectstyle.com>
2021-11-06 16:47:26 +0200

    Jetty 11 support #111
    .. porting junit5 module

---
+++ bootique-jetty11-junit5/src/test/java/io/bootique/jetty/v11/junit5/JettyTester_DefaultConfigIT.java
@@ -0,0 +1,83 @@
+/*
+ * Licensed to ObjectStyle LLC under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ObjectStyle LLC licenses
+ * this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package io.bootique.jetty.v11.junit5;
+
+import io.bootique.BQCoreModule;
+import io.bootique.BQRuntime;
+import io.bootique.Bootique;
+import io.bootique.jetty.v11.JettyModule;
+import io.bootique.junit5.BQApp;
+import io.bootique.junit5.BQTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@BQTest
+public class JettyTester_DefaultConfigIT {
+
+    private static final String OUT_CONTENT = "____content_stream____";
+
+    static final JettyTester jetty = JettyTester.create();
+
+    @BQApp
+    static final BQRuntime app = Bootique.app("-s")
+            .autoLoadModules()
+            .module(b -> JettyModule.extend(b).addServlet(ContentServlet.class))
+            .module(jetty.moduleReplacingConnectors())
+            // for predictable URL assertions
+            .module(b -> BQCoreModule.extend(b).setProperty("bq.jetty.connectors[0].host", "127.0.0.1"))
+            .createRuntime();
+
+    @Test
+    public void testGetServerUrl() {
+        String url = jetty.getUrl();
+        Assertions.assertNotNull(url);
+        assertEquals("http://127.0.0.1:" + jetty.getPort(), url);
+    }
+
+    @Test
+    public void testGetClient() {
+        WebTarget client = jetty.getTarget();
+        Assertions.assertNotNull(client);
+
+        assertEquals("http://127.0.0.1:" + jetty.getPort(), client.getUri().toString());
+
+        Response r = client.request().get();
+        assertEquals(Response.Status.OK.getStatusCode(), r.getStatus());
+        assertEquals(OUT_CONTENT, r.readEntity(String.class));
+    }
+
+    @WebServlet(urlPatterns = "/*")
+    static class ContentServlet extends HttpServlet {
+
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+            resp.getWriter().append(OUT_CONTENT);
+        }
+    }
+}
---
+++ bootique-jetty11-junit5/src/test/java/io/bootique/jetty/v11/junit5/JettyTester_MatcherIT.java
@@ -0,0 +1,129 @@
+/*
+ * Licensed to ObjectStyle LLC under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ObjectStyle LLC licenses
+ * this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package io.bootique.jetty.v11.junit5;
+
+import io.bootique.BQRuntime;
+import io.bootique.Bootique;
+import io.bootique.jetty.v11.JettyModule;
+import io.bootique.junit5.BQApp;
+import io.bootique.junit5.BQTest;
+import org.junit.jupiter.api.Test;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@BQTest
+public class JettyTester_MatcherIT {
+
+    private static final String OUT_CONTENT = "____content_stream____";
+
+    static final JettyTester jetty = JettyTester.create();
+
+    @BQApp
+    static final BQRuntime app = Bootique.app("-s")
+            .autoLoadModules()
+            .module(b -> JettyModule.extend(b).addServlet(ContentServlet.class))
+            .module(jetty.moduleReplacingConnectors())
+            .createRuntime();
+
+    @Test
+    public void testMatch200() {
+        Response r1 = jetty.getTarget().request().get();
+        JettyTester.matcher(r1).assertOk().assertContent(OUT_CONTENT);
+
+        // try a different style
+        Response r2 = jetty.getTarget().request().get();
+        JettyTester.assertOk(r2).assertContent(OUT_CONTENT);
+    }
+
+    @Test
+    public void testMatch200_ContentType() {
+        Response r1 = jetty.getTarget().queryParam("wantedType", "application/json").request().get();
+        JettyTester.matcher(r1).assertOk().assertContentType(MediaType.APPLICATION_JSON_TYPE);
+
+        // non-standard type
+        Response r2 = jetty.getTarget().queryParam("wantedType", "application/geo+json").request().get();
+        JettyTester.matcher(r2).assertOk().assertContentType(new MediaType("application", "*"));
+
+        // string comparision
+        Response r3 = jetty.getTarget().queryParam("wantedType", "application/geo+json").request().get();
+        JettyTester.matcher(r3).assertOk().assertContentType("application/*");
+    }
+
+    @Test
+    public void testMatch200_CustomAssertions() {
+        Response r = jetty.getTarget().request().get();
+        JettyTester.matcher(r).assertOk().assertContent(c -> {
+            assertNotNull(c);
+            assertTrue(c.contains("_stream_"));
+        });
+    }
+
+    @Test
+    public void testMatch400() {
+        Response r1 = jetty.getTarget().queryParam("wantedStatus", "404").request().get();
+        JettyTester.matcher(r1).assertNotFound();
+
+        // try a different style
+        Response r2 = jetty.getTarget().queryParam("wantedStatus", "404").request().get();
+        JettyTester.assertNotFound(r2);
+    }
+
+    @Test
+    public void testMatch500() {
+        Response r1 = jetty.getTarget().queryParam("wantedStatus", "500").request().get();
+        JettyTester.matcher(r1).assertStatus(500);
+
+        // try a different style
+        Response r2 = jetty.getTarget().queryParam("wantedStatus", "500").request().get();
+        JettyTester.assertStatus(r2, 500);
+    }
+
+    @WebServlet(urlPatterns = "/*")
+    static class ContentServlet extends HttpServlet {
+
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+            String statusString = req.getParameter("wantedStatus");
+            int status = statusString != null ? Integer.parseInt(statusString) : 200;
+
+            if(status == 200) {
+
+                String contentType = req.getParameter("wantedType");
+                if(contentType != null) {
+                    resp.setContentType(contentType);
+                }
+
+                resp.getWriter().append(OUT_CONTENT);
+            }
+            else {
+                resp.setStatus(status);
+            }
+        }
+    }
+}
